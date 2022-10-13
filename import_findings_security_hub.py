21afd1dbf4ded89dbffa9fb40d92e3e5465d2c5d
Abhishek bv <61512598+bvabhishek@users.noreply.github.com>
2022-10-13 15:36:43 +0530

    Update import_findings_security_hub.py

--- import_findings_security_hub.py
+++ import_findings_security_hub.py
@@ -1,10 +1,8 @@
-from imp import source_from_cache
 import os
 import json
 import logging
-import boto3
-import securityhub
 from datetime import datetime, timezone
+import codecs
 
 FINDING_TITLE = "CodeAnalysis"
 account_id = "449630918120"
@@ -12,34 +10,79 @@ region = "us-west-2"
 source_repository = "ASE-ZAP-Security-Hub"
 source_branch = "main"
 source_commitid = "1"
-build_id = "1"
+build_id = "VulFlask ZAP Scan"
 report_url = "https://aws.amazon.com"
 report_type = "OWASP-Zap code scan"
 generator_id = f"{report_type.lower()}-{source_repository}-{source_branch}"
 finding_type = "OWASP-Zap code scan"
-report_type = "OWASP-Zap"
+report_type = "OWASPZap"
 BEST_PRACTICES_OWASP = "https://owasp.org/www-project-top-ten/"
+created_at = datetime.now(timezone.utc).isoformat()
 
-def import_findings_security_hub(event):
-    severity = 50
-    alert_ct = event['report']['site'][0]['alerts']
-    alert_count = len(alert_ct)
-    for alertno in range(alert_count):
-        risk_desc = event['report']['site'][0]['alerts'][alertno]['riskdesc']
-        riskletters = risk_desc[0:3]
-        if riskletters == 'Hig':
-            normalized_severity = 70
-        elif riskletters == 'Med':
-            normalized_severity = 60
-        elif riskletters == 'Low' or riskletters == 'Inf':  
-            normalized_severity = 30
-        else:
-            normalized_severity = 90                                       
-        instances = len(event['report']['site'][0]['alerts'][alertno]['instances'])
-        finding_description = f"{alertno}-Vulerability:{event['report']['site'][0]['alerts'][alertno]['alert']}-Total occurances of this issue:{instances}"
-        finding_id = f"{alertno}-{report_type.lower()}-{build_id}"
-        created_at = datetime.now(timezone.utc).isoformat()
-        ### Calling Securityhub function to post the findings
-        securityhub.import_finding_to_sh(alertno, account_id, region, created_at, source_repository, source_branch, source_commitid, build_id, report_url, finding_id, generator_id, normalized_severity, severity, finding_type, FINDING_TITLE, finding_description, BEST_PRACTICES_OWASP)
+zap_aws_sechub_data = []
 
-import_findings_security_hub("/zap_results/VulFlask_parametrized_zap_scan.json")
+def import_findings_security_hub(json_file):
+    with codecs.open(json_file,'r',encoding='utf-8') as fp:
+        datafile = json.load(fp)
+        severity = 50
+        alerts = datafile['Report']['Sites']
+        alertno = 1
+        for alert in alerts:
+            risk_desc = alert['Alerts']['AlertItem']['RiskDesc']
+            riskletters = risk_desc[0:3]
+            if riskletters == 'Hig':
+                normalized_severity = 70
+            elif riskletters == 'Med':
+                normalized_severity = 60
+            elif riskletters == 'Low' or riskletters == 'Inf':  
+                normalized_severity = 30
+            else:
+                normalized_severity = 90
+            Description = alert['Alerts']['AlertItem']['Desc']
+            Solution = alert['Alerts']['AlertItem']['Solution']
+            Title = alert['Alerts']['AlertItem']['Alert']
+            CWEID = alert['Alerts']['AlertItem']['CWEID'] or 0
+
+            finding_id = f"{alertno}{report_type.lower()}"
+            
+            int_data = {
+                "SchemaVersion": "2018-10-08",
+                "Id": finding_id,
+                "ProductArn": "arn:aws:securityhub:{0}:{1}:product/{1}/default".format(region, account_id),
+                "GeneratorId": generator_id,
+                "AwsAccountId": account_id,
+                "Types": [
+                    "Software and Configuration Checks/AWS Security Best Practices/{0}".format(
+                        finding_type)
+                ],
+                "CreatedAt": created_at,
+                "UpdatedAt": created_at,
+                "Severity": {
+                    "Normalized": normalized_severity,
+                },
+                "Title":  Title,
+                "Description": Description,
+                'Remediation': {
+                    'Recommendation': {
+                        'Text': Solution,
+                        'Url': 'https://cwe.mitre.org/data/definitions/%s.html'%CWEID
+                    }
+                },
+                'SourceUrl': 'https://cwe.mitre.org/data/definitions/%s.html'%CWEID,
+                'Resources': [
+                    {
+                        'Id': build_id,
+                        'Type': "ZAP",
+                        'Partition': "aws",
+                        'Region': region
+                    }
+                ],
+            }
+            zap_aws_sechub_data.append(int_data)
+            alertno = alertno + 1
+
+    print(zap_aws_sechub_data)
+
+
+
+import_findings_security_hub("zap_report.json")
