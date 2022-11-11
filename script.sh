490a4d3c32c03b22e36e77ff462bc04e59f84a9f
Abhishek bv <61512598+bvabhishek@users.noreply.github.com>
2022-11-11 13:12:07 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-aws ecr get-login-password --region us-west-2 | docker login  --username AWS  --password-stdin $(terraform output --raw password)
-docker build -t $(terraform output --raw ecrreponame) -f Dockerfile .
-docker tag $(terraform output --raw ecrreponame):latest $(terraform output --raw ecrrepourl):latest
-docker push $(terraform output --raw ecrrepourl):latest
